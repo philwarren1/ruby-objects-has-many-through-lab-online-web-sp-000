fb9a9d7f43ad1aeda5f04d35c2cd230ccc837857
Philip Warren <philwarren1@gmail.com>
2020-08-14 14:35:26 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,10 +1,18 @@
 class Appointment
-  attr_accessor :date, :doctor, :patient
 
-  def initialize(date, doctor)
+  attr_accessor :date, :patient, :doctor
+
+  @@all = []
+
+  def initialize(date, patient, doctor)
     @date = date
+    @patient = patient
     @doctor = doctor
-    doctor.add_appointment(self)
+    @@all << self
   end
 
-end 
+  def self.all
+    @@all
+  end
+
+end
